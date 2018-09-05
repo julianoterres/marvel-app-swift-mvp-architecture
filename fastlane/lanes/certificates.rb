e3b6162bc0791d9a412f007dfd54056648b40ba3
Juliano Terres <juliano@julianoterres.com.br>
2018-09-05 16:15:07 -0300

    Fix fastlane file

--- fastlane/lanes/certificates.rb
+++ fastlane/lanes/certificates.rb
@@ -108,13 +108,13 @@ lane :get_certificates_and_provisioning_profile do
   )
 
   # Install the provisionig profile
-  if !generateNewCertificate {
+  if !generateNewCertificate
 
     install_provisioning_profile(
       certificateId: certificateId
     )
 
-  }
+  end
 
 end
 
