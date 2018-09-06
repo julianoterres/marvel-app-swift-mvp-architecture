977d1027300d39f282515e45bb1a2d7dea8ed002
Juliano Terres <juliano@julianoterres.com.br>
2018-09-06 16:30:15 -0300

    Change configs keychain

--- fastlane/lanes/jenkins.rb
+++
@@ -1,9 +0,0 @@
-# Unlock keychain
-lane :keychain_unlock do
-
-  unlock_keychain(
-    path: File.expand_path(ENV['PATH_KEYCHAIN']+ENV['KEYCHAIN_FILE_NAME']),
-    password: ENV['CERTIFICATE_SIGNING_FILE_PASSWORD']
-  )
-  
-end
