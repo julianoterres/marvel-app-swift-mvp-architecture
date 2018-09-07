7d224cbe7fe944b075b519de681fdf7cde29b23d
Juliano Terres <juliano@julianoterres.com.br>
2018-09-06 23:51:04 -0300

    Change keychan

--- fastlane/lanes/keychain.rb
+++ fastlane/lanes/keychain.rb
@@ -1,12 +1,14 @@
 # Unlock keychain
 lane :keychain_unlock do
 
-  if Helper.ci?
-    
-    Dir.chdir ".." do
-      sh('security set-key-partition-list -S apple-tool:,apple: -s -k 1q2w3e4r '+ File.expand_path(ENV['PATH_KEYCHAIN'] + 'login.keychain-db'))
-    end
+  sh('security set-key-partition-list -S apple-tool:,apple: -s -k ' + ENV["CERTIFICATE_SIGNING_FILE_PASSWORD"] + ' '+ File.expand_path(ENV['PATH_KEYCHAIN']+ENV['KEYCHAIN_FILE_NAME']+'-db'))
 
-  end
+  # if Helper.ci?
+    
+  #   Dir.chdir ".." do
+  #     sh('security set-key-partition-list -S apple-tool:,apple: -s -k 1q2w3e4r '+ File.expand_path(ENV['PATH_KEYCHAIN'] + 'login.keychain-db'))
+  #   end
+
+  # end
 
 end
