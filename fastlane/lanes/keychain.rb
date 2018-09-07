372bac5546cd1b5798a1dff0f4a0ba9d8ef8d5d0
Juliano Terres <juliano@julianoterres.com.br>
2018-09-06 23:01:26 -0300

    Fix keychain when is execute by jenkins

--- fastlane/lanes/keychain.rb
+++ fastlane/lanes/keychain.rb
@@ -1,7 +1,7 @@
 # Unlock keychain
 lane :keychain_unlock do
 
-  if ENV['IS_JENKINS'] == 'yes'
+  if Helper.ci?
     
     Dir.chdir ".." do
       sh('security set-key-partition-list -S apple-tool:,apple: -s -k 1q2w3e4r '+ File.expand_path(ENV['PATH_KEYCHAIN'] + 'login.keychain-db'))
