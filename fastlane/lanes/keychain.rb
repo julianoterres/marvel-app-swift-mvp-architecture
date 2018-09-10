a93729895c400ce54346b4a2aafe7960a1de57c3
Juliano Terres <juliano@julianoterres.com.br>
2018-09-10 12:52:08 -0300

    Add keychain when jenkins run job

--- fastlane/lanes/keychain.rb
+++ fastlane/lanes/keychain.rb
@@ -8,12 +8,13 @@ lane :keychain_unlock do
 
   end
 
-  # if Helper.ci?
+  # Check if is jenkins
+  if Helper.ci?
     
-  #   Dir.chdir ".." do
-  #     sh('security set-key-partition-list -S apple-tool:,apple: -s -k 1q2w3e4r '+ File.expand_path(ENV['PATH_KEYCHAIN'] + 'login.keychain-db'))
-  #   end
+    Dir.chdir ".." do
+      sh('security set-key-partition-list -S apple-tool:,apple: -s -k 1q2w3e4r '+ File.expand_path(ENV['PATH_KEYCHAIN'] + 'login.keychain-db'))
+    end
 
-  # end
+  end
 
 end
